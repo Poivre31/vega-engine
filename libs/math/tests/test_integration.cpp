e7166b4daf189f2677bdca5b4d7720ac41188d9a
Poivre31 <poivre.astro@gmail.com>
2026-07-11 04:38:02 +0200

    Added benchmark support via nanobench library Includes a bench example of trapezoidal integration Small reorganisation of math library

---
+++ libs/math/tests/test_integration.cpp
@@ -0,0 +1,19 @@
+#include "math/trapezium.h"
+#include <cmath>
+#include <functional>
+
+#include <gtest/gtest.h>
+
+TEST(TestMath, TestIntegrate) {
+  double a = -.5;
+  double b = 3;
+  size_t n = 100;
+
+  double max_error =
+      abs((b - a) * (b - a) * (b - a) / (12 * n * n) *
+          exp(-std::min(a, b))); //> Estimated error for trapezoidal rule
+
+  EXPECT_NEAR(integration::trapezium(
+                  a, b, [](double x) { return exp(-x); }, n),
+              exp(-a) - exp(-b), 1.1 * max_error);
+}
